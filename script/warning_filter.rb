b76842c79f8436a65880867bedb62af75470d6cd
Chris Alef <chris@crickertech.com>
2025-11-23 14:51:32 -0800

    Suppress third-party module warnings when building so we can see things that actually break

---
+++ script/warning_filter.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+#
+# Silences noisy warnings emitted by upstream gems when Ruby runs with -w.
+
+module WarningFilter
+  SUPPRESSED_PATTERNS = [
+    %r{/gems/icalendar-[^/]+/lib/icalendar/downcased_hash\.rb:.*`&' interpreted as argument prefix},
+    %r{/gems/kramdown-[^/]+/lib/kramdown/parser/base\.rb:.*character class has duplicated range},
+    %r{/gems/jekyll-paginate-v2-[^/]+/lib/jekyll-paginate-v2/generator/paginator\.rb:.*warning: method redefined},
+    %r{/lib/ruby/\d+\.\d+\.\d+/forwardable\.rb:.*warning: method redefined},
+    %r{/gems/rouge-[^/]+/lib/rouge/lexers/.*: warning:},
+    %r{/gems/rouge-[^/]+/lib/rouge/lexers/julia\.rb:.*character class has duplicated range},
+    %r{/gems/rouge-[^/]+/lib/rouge/lexers/yaml\.rb:.*warning: method redefined}
+  ].freeze
+
+  def self.ignore?(message)
+    SUPPRESSED_PATTERNS.any? { |pattern| message.match?(pattern) }
+  end
+end
+
+Warning.singleton_class.prepend(Module.new do
+  def warn(message)
+    return if WarningFilter.ignore?(message)
+
+    super
+  end
+end)
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'simplecov'
+require_relative '../script/warning_filter'
 
 SimpleCov.start do
   enable_coverage :branch
