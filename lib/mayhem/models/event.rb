5116c44d46003a379a7588e0295f516650c317d3
Chris Alef <chris@crickertech.com>
2025-12-20 02:29:04 -0800

    add attribute accessors and predicate helpers for Event along with corresponding coverage (#335)

--- lib/mayhem/models/event.rb
+++ lib/mayhem/models/event.rb
@@ -17,12 +17,56 @@ module Mayhem
         self['end_date']
       end
 
+      def feed_content
+        self['feed_content']
+      end
+
+      def feed_content_checksum
+        self['feed_content_checksum']
+      end
+
+      def generated_from_post
+        self['generated_from_post']
+      end
+
+      def generated_from_post?
+        self['generated_from_post'] == true
+      end
+
+      def images
+        self['images']
+      end
+
       def location
         self['location']
       end
 
+      def locations
+        self['locations']
+      end
+
+      def locked
+        self['locked']
+      end
+
+      def locked?
+        self['locked'] == true
+      end
+
       def organization_title
         self['organization_title']
+      end
+
+      def original_source_html
+        self['original_source_html']
+      end
+
+      def published
+        self['published']
+      end
+
+      def published?
+        self['published'] != false
       end
 
       def start_date
@@ -33,8 +77,20 @@ module Mayhem
         self['source_url']
       end
 
+      def summarized
+        self['summarized']
+      end
+
+      def summarized?
+        self['summarized'] == true
+      end
+
       def title
         self['title']
+      end
+
+      def topics
+        self['topics']
       end
     end
   end
