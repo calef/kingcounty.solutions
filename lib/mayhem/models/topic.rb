2103bcfd1aa1f627b5b6d5969c5d9d4e6bc689bb
Chris Alef <chris@crickertech.com>
2025-12-26 19:04:33 -0800

    implement Topic organizations, news, and events defs

--- lib/mayhem/models/topic.rb
+++ lib/mayhem/models/topic.rb
@@ -14,7 +14,31 @@ module Mayhem
         "_topics/#{slug}.md"
       end
 
-      # TODO: add organizations, news, events def to get all models associated with this topic
+      def organizations
+        require_relative 'organization'
+        related_records(Mayhem::Models::Organization)
+      end
+
+      def news
+        require_relative 'news'
+        related_records(Mayhem::Models::News)
+      end
+
+      def events
+        require_relative 'event'
+        related_records(Mayhem::Models::Event)
+      end
+
+      private
+
+      def related_records(model)
+        title_value = title.to_s.strip
+        return [] if title_value.empty?
+
+        model.all.select do |record|
+          Array(record.topic_titles).map(&:to_s).map(&:strip).include?(title_value)
+        end
+      end
     end
   end
 end
