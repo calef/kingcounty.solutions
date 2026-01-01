0bd915472445983e9230431db6597a88cc257777
Chris Alef <chris@crickertech.com>
2025-12-31 19:35:44 -0800

    added Mayhem::Models::RobotsTxt

---
+++ lib/mayhem/models/robots_txt.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'fmrepo'
+require 'uri'
+require_relative 'abstract_sitemap'
+
+module Mayhem
+  module Models
+    class RobotsTxt < AbstractSitemap
+      COLLECTION_DIR = '_robots_txts'
+
+      repository_role :robots_txts
+      scope glob: "#{COLLECTION_DIR}/**/*.{md,markdown}"
+      naming do |front_matter:, **|
+        url = front_matter['url']
+        slug_source = extract_domain(url) || url
+        slug_source = 'untitled' if slug_source.to_s.strip.empty?
+        slug = FMRepo.slugify(slug_source)
+        "#{COLLECTION_DIR}/#{slug}.md"
+      end
+
+      def self.extract_domain(url)
+        return unless url
+
+        uri = URI.parse(url.to_s)
+        return unless uri.host
+
+        host = uri.host
+        port_suffix = uri.port && uri.port != uri.default_port ? "-#{uri.port}" : ''
+        "#{host}#{port_suffix}"
+      rescue URI::InvalidURIError
+        nil
+      end
+    end
+  end
+end
